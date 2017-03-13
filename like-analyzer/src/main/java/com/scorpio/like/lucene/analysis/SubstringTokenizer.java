6524b8dfb6c9eaa796b2a9d5bb39ac92103c8856
tengzhe.ln <tengzhe.ln@alibaba-inc.com>
2017-03-13 22:04:24 +0800

    优化读性能

--- like-analyzer/src/main/java/com/scorpio/like/lucene/analysis/SubstringTokenizer.java
+++ like-analyzer/src/main/java/com/scorpio/like/lucene/analysis/SubstringTokenizer.java
@@ -7,11 +7,9 @@ import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * 任意字符子串匹配
+ * 匹配字符任意子串
  *
  * @author skyfalling
  */
@@ -20,25 +18,38 @@ public class SubstringTokenizer extends Tokenizer {
   private final PositionIncrementAttribute posIncrAtt = addAttribute(PositionIncrementAttribute.class);
   private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
   private int offset = -1;
-  private List<Character> buffer = new ArrayList<Character>();
+  private StringBuilder buffer = new StringBuilder();
 
 
+  /**
+   * 重置偏移量
+   *
+   * @throws IOException
+   */
   @Override
   public void reset() throws IOException {
     //must call super reset first
     super.reset();
+    //重置偏移量
     this.offset = -1;
   }
 
+  /**
+   * 重新{@link super#incrementToken()}方法,每个字符一个term,偏移量加1
+   *
+   * @return
+   * @throws IOException
+   */
   @Override
   public final boolean incrementToken() throws IOException {
     clearAttributes();
+    //after reset called, offset is -1, and then read full input
     if (offset == -1) {
       readFully(input);
     }
     offset++;
-    if (offset < buffer.size()) {
-      termAtt.setEmpty().append(buffer.get(offset));
+    if (offset < buffer.length()) {
+      termAtt.setEmpty().append(buffer.charAt(offset));
       posIncrAtt.setPositionIncrement(1);
       offsetAtt.setOffset(offset, offset + 1);
       return true;
@@ -47,14 +58,18 @@ public class SubstringTokenizer extends Tokenizer {
   }
 
 
+  /**
+   * 读取并缓存数据源
+   *
+   * @param reader
+   * @throws IOException
+   */
   private void readFully(Reader reader) throws IOException {
-    this.buffer.clear();
-    int length = -1;
-    char[] readed = new char[64];
-    while ((length = reader.read(readed)) != -1) {
-      for (int i = 0; i < length; i++) {
-        this.buffer.add(readed[i]);
-      }
+    this.buffer.setLength(0);
+    int length;
+    char[] read = new char[64];
+    while ((length = reader.read(read)) != -1) {
+      this.buffer.append(read, 0, length);
     }
   }
 }
